2e9b0725890e74bdc71e4b7c5de4983e11ee588a
Marcin <contact@mmskl.net>
2023-03-22 03:53:00 +0100

    Add queueList to queue controller

--- lib/controllers/queue_controller.dart
+++ lib/controllers/queue_controller.dart
@@ -1,6 +1,40 @@
 import 'package:get/get.dart';
+import 'package:uuid/uuid.dart';
 
-class QueueController extends GetxController {
-  final String title = 'Test string title from controller';
+import '../models/item_model.dart';
+import './with_player_controller.dart';
+
+class QueueController extends WithPlayerController {
+
+  Rx<List<ItemModel>>queueList = Rx<List<ItemModel>>([]);
+  Rx<int> queueCount = 0.obs;
+
+  void addToQueueList(String title, String author, String url) {
+
+    var q = ItemModel(
+        id: Uuid(),
+        title: title,
+        author: author,
+        url: url,
+      );
+
+    this.queueList.value.add(q);
+    this.queueCount.value = queueList.value.length;
+
+    this.queueCount.refresh();
+    this.queueList.refresh();
+
+
+  }
+
+
+  void play(Uuid? id) {
+    var _queueItem = queueList.value.firstWhere((item) => (item.id == id));
+    print(_queueItem);
+    print(_queueItem.title);
+    print(_queueItem.author);
+    print(_queueItem.toString());
+    this.play_now(_queueItem);
+  }
+
 }
-
